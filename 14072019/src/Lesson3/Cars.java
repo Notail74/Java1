a40fadc70831653a2d0ca245e05bdcc4212a6316
Protsenko Maksim <net3311971@icloud.com>
2019-08-14 20:32:14 +0700

    initial commit comments

--- 14072019/src/Lesson3/Cars.java
+++ 14072019/src/Lesson3/Cars.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Cars {
     //Создать массив с набором слов (10-20 слов, среди которых должны встречаться повторяющиеся). Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.
     public static void main(String[] args) {
+        //2 решение ДЗ
         String[] cars = {"Volvo", "VAG", "BMW", "Volvo"};
         HashMap<String, Integer> service = new HashMap<>();
         for (String car : cars) {
